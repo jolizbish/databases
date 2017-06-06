a6d394e0c0f80ed89c886e0ae9a060b06718c282
Hack Reactor Student <student@hackreactor.com>
2017-06-06 17:14:18 -0500

    ORM refactor based on solution code

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,44 +1,81 @@
-var models = require('../models');
+var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function (req, res) {
-      models.messages.get(function(error, data) {
-        if (error) {
-          throw error;
-        }
-        res.json(data);
+    get: function(req, res) {
+      db.Message.findAll({include: [db.User]})
+      .then(function(messages) {
+        res.json(messages);
       });
-    }, // a function which handles a get request for all messages
-    post: function (req, res) {
-      var params = [req.body.message, req.body.username, req.body.roomname];
-      models.messages.post(params, function(error, data) {
-        if (error) {
-          throw error;
-        }
-        res.sendStatus(201);
+    },
+    post: function(req, res) {
+      db.User.findOrCreate({where: {username: req.body.username}})
+      .spread(function(user, created){
+        db.Message.create({userid: user.get('id'), message: req.body.message, roomname: req.body.roomname})
+        .then(function(message){
+          res.sendStatus(201);
+        });
       });
-    } // a function which handles posting a message to the database
+    }
   },
 
   users: {
-    // Ditto as above
-    get: function (req, res) {
-      models.users.get(function(error, data) {
-        if (error) {
-          throw error;
-        }
-        res.json(data);
+    get: function(req, res) {
+      db.User.findAll().then(function(users){
+        res.json(users);
       });
     },
-    post: function (req, res) {
-      var params = [req.body.username];
-      models.users.post(params, function(error, data) {
-        if (error) {
-          throw error;
-        }
-        res.sendStatus(201);
+    post: function(req, res) {
+      db.User.findOrCreate({where: {username: req.body.username}})
+      .spread(function(user, created) {
+        res.sendStatus(created ? 201 : 200);
       });
     }
   }
 };
+
+
+// var models = require('../models');
+//
+// module.exports = {
+//   messages: {
+//     get: function (req, res) {
+//       models.messages.get(function(error, data) {
+//         if (error) {
+//           throw error;
+//         }
+//         res.json(data);
+//       });
+//     }, // a function which handles a get request for all messages
+//     post: function (req, res) {
+//       var params = [req.body.message, req.body.username, req.body.roomname];
+//       models.messages.post(params, function(error, data) {
+//         if (error) {
+//           throw error;
+//         }
+//         res.sendStatus(201);
+//       });
+//     } // a function which handles posting a message to the database
+//   },
+//
+//   users: {
+//     // Ditto as above
+//     get: function (req, res) {
+//       models.users.get(function(error, data) {
+//         if (error) {
+//           throw error;
+//         }
+//         res.json(data);
+//       });
+//     },
+//     post: function (req, res) {
+//       var params = [req.body.username];
+//       models.users.post(params, function(error, data) {
+//         if (error) {
+//           throw error;
+//         }
+//         res.sendStatus(201);
+//       });
+//     }
+//   }
+// };
--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,37 @@
-var mysql = require('mysql');
+var Sequelize = require('sequelize');
+var db = new Sequelize('chat', 'student', 'student');
+
+var User = db.define('User', {
+  username: Sequelize.STRING
+});
+
+var Message = db.define('Message', {
+  message: Sequelize.STRING,
+  roomname: Sequelize.STRING
+});
+
+Message.belongsTo(User);
+
+User.hasMany(Message);
+
+User.sync();
+Message.sync();
+
+exports.user = User;
+exports.message = Message;
 
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'student',
-  password : 'student',
-  database : 'chat'
-});
-
-connection.connect()
-
-module.exports = connection;
+//
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'student',
+//   password : 'student',
+//   database : 'chat'
+// });
+//
+// connection.connect()
+//
+// module.exports = connection;
