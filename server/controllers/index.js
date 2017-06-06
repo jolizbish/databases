a2aecc097642bbc0ac4176c9213ffc4f41b013e6
Hack Reactor Student <student@hackreactor.com>
2017-06-06 12:04:47 -0500

    broke everything, reverting to last commit

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -13,6 +13,7 @@ module.exports = {
       });
     }, // a function which handles a get request for all messages
     post: function (req, res) {
+      console.log('POSTING MESSAGE CONTROLLER', req.query);
       models.messages.post(req.query, function(error, data) {
         if (error) {
           throw error;
@@ -22,13 +23,24 @@ module.exports = {
     } // a function which handles posting a message to the database
   },
 
-  users: {
-    // Ditto as above
-    get: function (req, res) {
-
-    },
-    post: function (req, res) {
-
-    }
-  }
+  // users: {
+  //   // Ditto as above
+  //   get: function (req, res) {
+  //     models.users.get(null, function(error, data) {
+  //       if (error) {
+  //         throw error;
+  //       } else {
+  //         res.send(data);
+  //       }
+  //     });
+  //   },
+  //   post: function (req, res) {
+  //     models.users.post(req.query, function(error, data) {
+  //       if (error) {
+  //         throw error;
+  //       }
+  //       res.json(data);
+  //     })
+  //   }
+  // }
 };
--- server/schema.sql
+++ server/schema.sql
@@ -1,27 +1,34 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+
+
 CREATE TABLE messages (
   /* Describe your table here.*/
   id INTEGER NOT NULL AUTO_INCREMENT,
-  username VARCHAR(30),  --COME BACK TO THIS AND FIGURE OUT JOIN TABLE
+  username VARCHAR(30),
   message VARCHAR(140),
   roomname VARCHAR(30),
   createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
   updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY(id),
-);
-
-/* Create other tables and define schemas for them here! */
-
-CREATE TABLE users (
-
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  username VARCHAR(30),
   PRIMARY KEY(id)
 
-)
+);
+
+-- CREATE TABLE users (
+--
+--   id INTEGER NOT NULL AUTO_INCREMENT,
+--   username VARCHAR(30) UNIQUE,
+--   PRIMARY KEY(id)
+--
+-- );
+
+-- FOREIGN KEY (user_id) REFERENCES users(id)
+
+
+/* Create other tables and define schemas for them here! */
 
 
 /*  Execute this file from the command line by typing:
